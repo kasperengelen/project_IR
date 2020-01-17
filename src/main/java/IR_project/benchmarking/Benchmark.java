d5a14a5584488c151f1e2434c8eb90040dcf67b2
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2020-01-17 17:19:07 +0100

    Working on benchmark

--- src/main/java/IR_project/benchmarking/Benchmark.java
+++ src/main/java/IR_project/benchmarking/Benchmark.java
@@ -21,7 +21,7 @@ public class Benchmark
     public static void titleQueryBM(Searcher s, String sim) {
         // Open ground truth
         try {
-            BufferedReader br = new BufferedReader(new FileReader("./groundTruth/titleToDoc.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("./groundTruth/titleToDoc100K.txt"));
 
             PrintWriter rank_out = new PrintWriter(new File("./titleQueryRank" + sim + ".txt"));
 
@@ -33,7 +33,7 @@ public class Benchmark
                 String query = line.substring(0, splitIdx-1);
                 String docID = line.substring(splitIdx+2).toLowerCase().replace(".xml", "").toUpperCase();
 
-                Searcher.SearchResult result = s.search(query, 20);
+                Searcher.SearchResult result = s.search(query, 100);
                 int rank = result.topResultIDs.indexOf(docID) + 1;
                 rank_out.println(rank);
 
@@ -55,25 +55,25 @@ public class Benchmark
     public static void main(String[] argv)
     {
         Logger.logDebug("Initializing search engines");
-//        Searcher s1 = new Searcher(Paths.get("../index/"), new BM25Similarity());
+        Searcher s1 = new Searcher(Paths.get("../index/"), new BM25Similarity());
         Searcher s2 = new Searcher(Paths.get("../index1/"), new ClassicSimilarity());
-//        Searcher s3 = new Searcher(Paths.get("../index2/"), new LMJelinekMercerSimilarity((float) 0.8));
-//        Searcher s4 = new Searcher(Paths.get("../index3/"),  new LMDirichletSimilarity((float) 0.8));
+        Searcher s3 = new Searcher(Paths.get("../index2/"), new LMJelinekMercerSimilarity((float) 0.8));
+        Searcher s4 = new Searcher(Paths.get("../index3/"),  new LMDirichletSimilarity((float) 0.8));
 
-//        Logger.logDebug("Started BM25 benchmark");
-//        titleQueryBM(s1, "BM25");
-//        Logger.logDebug("Completed BM25 benchmark");
-
+        Logger.logDebug("Started BM25 benchmark");
+        titleQueryBM(s1, "BM25");
+        Logger.logDebug("Completed BM25 benchmark");
+//
         Logger.logDebug("Started TF-IDF benchmark");
         titleQueryBM(s2, "TD-IDF");
         Logger.logDebug("Completed TF-IDF benchmark");
 
-//        Logger.logDebug("Started LM Jelinek-Mercer benchmark");
-//        titleQueryBM(s3, "LMJM");
-//        Logger.logDebug("Completed LM Jelinek-Mercer benchmark");
+        Logger.logDebug("Started LM Jelinek-Mercer benchmark");
+        titleQueryBM(s3, "LMJM");
+        Logger.logDebug("Completed LM Jelinek-Mercer benchmark");
 
-//        Logger.logDebug("Started LM Dirichlet benchmark");
-//        titleQueryBM(s4, "LMD");
-//        Logger.logDebug("Completed LM Dirichlet benchmark");
+        Logger.logDebug("Started LM Dirichlet benchmark");
+        titleQueryBM(s4, "LMD");
+        Logger.logDebug("Completed LM Dirichlet benchmark");
     }
 }
